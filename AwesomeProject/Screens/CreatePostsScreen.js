f3c3f3554aebb14634ea105baacbfac72a519d7c
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-07-25 23:26:05 +0300

    Articles1-7

--- AwesomeProject/Screens/CreatePostsScreen.js
+++ AwesomeProject/Screens/CreatePostsScreen.js
@@ -1,33 +1,74 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useEffect,} from "react";
+import { Alert, Text, TextInput, View, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform} from "react-native";
+import { SimpleLineIcons } from '@expo/vector-icons';
 import { Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
 
- function Postsscreen () {
+
+const Postsscreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
+  const [location, setLocation] = useState(null);
+
+  const Publication = async () => {
+ let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      const coords = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      setLocation(coords);
+
+    if (location) {navigation.navigate("Postscreen")
+    }
+    else {
+            Alert.alert("There is no geolocation");
+     }
+  }
+
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       await MediaLibrary.requestPermissionsAsync();
 
       setHasPermission(status === "granted");
     })();
   }, []);
 
-  if (hasPermission === null) {
+// useEffect(() => {
+//     (async () => {
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== "granted") {
+//         console.log("Permission to access location was denied");
+//       }
+//       let location = await Location.getCurrentPositionAsync({});
+//       const coords = {
+//         latitude: location.coords.latitude,
+//         longitude: location.coords.longitude,
+//       };
+//       setLocation(coords);
+//     })();
+// }, []);
+  
+    if (hasPermission === null) {
     return <View />;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
-  return (
-    <View style={styles.container}>
-
-      <Camera
+  
+  
+   return (
+     <View style={styles.containerBackground}>
+           <View style={styles.container}>
+      <View style={styles.cameracontainer}>
+        <Camera
         style={styles.camera}
         type={type}
         ref={setCameraRef}
@@ -63,27 +104,78 @@ import * as MediaLibrary from "expo-media-library";
           </TouchableOpacity>
         </View>
       </Camera>
+      </View>
+      <View><Text style={styles.cameratext}>Завантажте фото</Text>
+     
+      </View>
+          <KeyboardAvoidingView
+                         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <View style={styles.NameLocationInputStyle}>
+                        <TextInput placeholderTextColor={'#BDBDBD'} 
+                         style={[styles.inputText, styles.fontFamilyProject]}
+                        // value={email}
+                        // onChangeText={setEmail}
+                        placeholder="Назва..." />
+      </View>
+           <View style={styles.NameLocationInputStyle}>
+             <SimpleLineIcons style={styles.iconNameLocationInputStyle} size={24} name="location-pin" color="black" />
+                {/* <Icon name='SlLocationPin' color='#000' size={14}/> */}
+             <TextInput placeholderTextColor={'#BDBDBD'}
+         
+                         style={[styles.inputText, styles.fontFamilyProject]}
+                        // value={email}
+                        // onChangeText={setEmail}<h3> Lets go for a <FaBeer />? </h3>
+                        placeholder="Місцевість..." />
+      </View>          
+        
+                    <TouchableOpacity
+          style={styles.publicButton}
+          // onPress={() => Alert.alert('Показати пароль')}>
+             onPress={Publication}> 
+               {/* onPress={() => navigation.navigate("Postscreen")}>  */}
+                        <Text style={[styles.buttontext, styles.fontFamilyProject]}>{"Опублікувати"}</Text>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+      
        </View>
+    </View>
+
 
   );
 }
 
-export default Postsscreen
+export default Postsscreen;
 
 const styles = StyleSheet.create({
 
+  containerBackground: {
+      flex: 1 ,
+      backgroundColor: "white",
+  },
+
   container: {
-    flex: 0.5 ,
+      flex: 1 ,
        marginTop: 32,
-    alignItems: 'center',
     margin: 24,
-    backgroundColor: "white",
+      marginBottom: 32,
+      backgroundColor: "white",
+    overflow: "hidden",
+
+  },
+    cameracontainer: {
+      flex: 1,
+      width: 343,
+      height: 240,
+
+    alignItems: 'center',
+    backgroundColor: "#E8E8E8",
     borderWidth: 1,
-      borderColor: "#E8E8E8",
+    borderColor: "#E8E8E8",
     borderRadius: 16,
     overflow: "hidden",
 
   },
+
   camera: {
     flex: 1,
     width: 343,
@@ -126,6 +218,57 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 50,
   },
+  cameratext: {
+  fontFamily: 'Roboto_400Regular',
+    fontWeight: "regular",
+  textAlign: 'left',
+    fontSize: 16,
+    color: "#BDBDBD",
+  },
+  inputText: {
+  fontFamily: 'Roboto_400Regular',
+    fontWeight: "regular",
+  textAlign: 'left',
+    fontSize: 16,
+    color: "#212121",
+  },
+
+  NameLocationInputStyle: {
+    flexDirection: 'row',
+    // flexWrap: 'wrap',
+    width: 343,
+    height: 50,
+    marginTop: 16,
+    borderBottomWidth: 1,
+    borderColor: "#E8E8E8",
+     color: "#212121",
+    fontSize: 16,
+    fontWeight: "regular"
+  },
+  iconNameLocationInputStyle: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    color: "#BDBDBD",
+       paddingRight: 4,
+  },
+
+  publicButton: {
+    width: 343,
+    marginTop: 43,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 26,
+    backgroundColor: "#F6F6F6",
+    textAlign: "center",
+  },
+    buttontext: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+    fontWeight: 'regular',
+      color: "#BDBDBD",
+  },
+
 });
 
 
